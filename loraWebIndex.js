7ff63517a87b7a04f30beb82cb1075087a6d7060
JaeYoung Hwang <jaeyounghwang@JaeYoungui-MacBook-Pro.local>
2018-12-19 17:05:29 +0900

    Adding cover page and mgmt page

--- loraWebIndex.js
+++ loraWebIndex.js
@@ -5,6 +5,7 @@
 var fs = require('fs');
 var ejs = require('ejs');
 var http = require('http');
+var async = require('async');
 var express = require('express');
 var bodyParser = require('body-parser');
 var requestToServer = require('request');
@@ -12,45 +13,74 @@ var requestToServer = require('request');
 var loraWebIndex = express();
 loraWebIndex.use(bodyParser.json());
 loraWebIndex.use(bodyParser.urlencoded({extended: false}));
-loraWebIndex.use(express.static('CSS'));
 loraWebIndex.use(express.static('ajax'));
 loraWebIndex.use(express.static('bootstrap'));
 loraWebIndex.use(express.static('pages'));
-loraWebIndex.use(express.static('tuiGrid'));
 loraWebIndex.use(express.static('scripts'));
+loraWebIndex.use(express.static('styles'));
 
-// Connecting the oneM2M Web Tester page.
-loraWebIndex.post('/localTestingNode', function (request, mainResponse) {
+var requestToAnotherServer = require('request');
 
-    var resultObj = request.body;
-    var configObject = resultObj['testConfig'];
-    var testcaseNameArray = configObject['testcaseName'];
-    var testcaseParamArray = configObject['testcaseParam'];
+var retreiveExecution = function (containerName, callBackResponse) {
 
-    for(var i = 0; i < testcaseNameArray.length; i++)
-        console.log(testcaseParamArray[i]);
+    var targetURL = "http://203.253.128.161:7579/Mobius/iotParking/parkingSpot/" + containerName + "/status" + "/la";
+    console.log(targetURL);
 
-    requestToServer({
-        url :'http://192.168.214.129:62590/TesterNode',
-        method : 'POST',
+    requestToAnotherServer({
+        url: targetURL,
+        method: 'GET',
         json: true,
-        body: {
-            "TestcaseName" : testcaseNameArray,
-            "TestcaseParameter" : testcaseParamArray
-        }},function(error, response ,body) {
-        console.log("Entering the original server");
-
-        var jsonObject  = new Object( );
-        jsonObject.result = response.body;
-        console.log(response.body);
-
-        if(error) {
-            mainResponse.status(200).send(jsonObject);
-        } else {
-            mainResponse.status(200).send(jsonObject);
+        headers: { // Basic AE resource structure for registration
+            'Accept': 'application/json',
+            'X-M2M-RI': '12345',
+            'X-M2M-Origin': 'Origin',
+        }
+    }, function (error, oneM2MResponse, body) {
+        if(typeof(oneM2MResponse) !== 'undefined') {
+            var root = oneM2MResponse.body;
+            var contentInstance = root['m2m:cin'];
+            var creationTime = contentInstance['ct'];
+            console.log(containerName + " : " + creationTime);
+            callBackResponse(oneM2MResponse.statusCode);
         }
     });
+};
+
+
+// Server testing code
+loraWebIndex.get('/localLoraSensorsCollector', function (request, response) {
+    console.log("in ajax function")
+
+    for(var i = 1; i < 136; i++) {
+
+        async.waterfall([
+            function() {
+
+                var containerName;
+
+                if(i < 10) {
+                    containerName = "KETI00" + i;
+                } else if (i < 100) {
+                    containerName = "KETI0" + i;
+                } else {
+                    containerName = "KETI" + i;
+                }
+
+                retreiveExecution(containerName, function (statusCode) {
+                    if(statusCode == 200) {
+                        return;
+                    }
+                });
+            },
+        ], function (statusCode, result) {
+            if(statusCode) {
+            }
+        }); // End of async.waterfall
+    }
+
+    response.send('<h1>'+ 'Context-Aware auxiliary component' + '</h1>');
 });
+
 
 // Connecting the oneM2M Web Tester page.
 loraWebIndex.get('/loraWebHome', function (request, response) {
